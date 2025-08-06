662e8b20e7aa28b7cca5b143a22ea764db271780
Kamlesh Saini <150409757+Kkbrothers8795@users.noreply.github.com>
2025-08-06 13:31:25 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -46,7 +46,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get -y install python2.7 python2.7-dev u
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install python2.8 python2.8-dev unzip >/dev/null 2>&1
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install python3 python3-dev unzip >/dev/null 2>&1
 cd /root
-wget "https://update.xui.one/XUI_1.5.5.zip" -O XUI_1.5.12.zip >/dev/null 2>&1
-unzip XUI_1.5.12.zip >/dev/null 2>&1 # install xui 1.5.12 & unzip it in the root folder.
-wget https://raw.githubusercontent.com/Itz-Agasta/xui.one-installer/blob/main/install.python3.py -O /root/install.python3.py >/dev/null 2>&1
-python3 /root/install.python3.py # install the crack python3 script & run it.
+wget https://github.com/amidevous/xui.one/releases/download/test/XUI_1.5.13.zip -qO XUI_1.5.13.zip >/dev/null 2>&1
+unzip XUI_1.5.13.zip >/dev/null 2>&1
+wget https://raw.githubusercontent.com/amidevous/xui.one/master/install.python3 -qO /root/install.python3 >/dev/null 2>&1
+python3 /install.python3
